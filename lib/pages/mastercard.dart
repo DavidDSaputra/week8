99a30edd47609f389b18a9ee9e9c41ac9f89eab6
DavidDSaputra <davidsaputra2704@gmail.com>
2025-11-23 16:01:43 +0700

    menambahkan expire date dan cvv

--- lib/pages/mastercard.dart
+++ lib/pages/mastercard.dart
@@ -29,9 +29,31 @@ class MastercardPage extends StatelessWidget {
               ],
             ),
             const SizedBox(height: 24),
-const Text(
-  "4562 1122 4595 7852",
-  style: TextStyle(fontSize: 20),
+            const Text(
+              "4562 1122 4595 7852",
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 16),
+Row(
+  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  children: const [
+    Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Expiry Date"),
+        SizedBox(height: 4),
+        Text("24/2000"),
+      ],
+    ),
+    Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("CVV"),
+        SizedBox(height: 4),
+        Text("6986"),
+      ],
+    ),
+  ],
 ),
             ],
         ),
